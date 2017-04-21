8518b9bd8dcef76db00a26333d00eed8c39253f0
atheys <A.Theys@student.tudelft.nl>
2017-04-21 15:58:44 +0200

    First version Modules module added.

--- Deadalos/Modules.py
+++ Deadalos/Modules.py
@@ -10,20 +10,123 @@ Module imports.
 from ComplexShapes import TruncOctahedron as Toc
 
 """
-Life support (L) = 1 --> Green
-Habitation (H) = 2 --> Blue
-Manufacturing (M) = 3
-Storage (St) = 4
-Shielding (Sh) = 5
-Telemetry/Communications (Tc) = 6
+Life Support Module Object Class.
 """
-
 class LifeSupport(object):
     
-    def __init__(R):
+    """
+    Basic Constructor.
+    
+    @param: [center] center point of the module (Point).
+    @param: [R]      rib (length) of the module (int/float).
+    """
+    def __init__(self,center,R):
+        self.c = center
         self.R = float(R)
+        self.label = 'L'
         self.r = (1./3.)*self.R
         self.color = 'g'
-        self
-        
-        
+        self.mod = Toc(self.c,self.R)
+        return
+
+"""
+Habitation Module Object Class.
+"""
+class Habitation(object):
+    
+    """
+    Basic Constructor.
+    
+    @param: [center] center point of the module (Point).
+    @param: [R]      rib (length) of the module (int/float).
+    """
+    def __init__(self,center,R):
+        self.c = center
+        self.R = float(R)
+        self.label = 'H'
+        self.r = (1./3.)*self.R
+        self.color = 'b'
+        self.mod = Toc(self.c,self.R)
+        return
+
+"""
+Manufacturing Module Object Class.
+"""
+class Manufacturing(object):
+    
+    """
+    Basic Constructor.
+    
+    @param: [center] center point of the module (Point).
+    @param: [R]      rib (length) of the module (int/float).
+    """
+    def __init__(self,center,R):
+        self.c = center
+        self.R = float(R)
+        self.label = 'M'
+        self.r = (1./3.)*self.R
+        self.color = 'r'
+        self.mod = Toc(self.c,self.R)
+        return
+
+"""
+Storage Module Object Class.
+"""
+class Storage(object):
+    
+    """
+    Basic Constructor.
+    
+    @param: [center] center point of the module (Point).
+    @param: [R]      rib (length) of the module (int/float).
+    """
+    def __init__(self,center,R):
+        self.c = center
+        self.R = float(R)
+        self.label = 'St'
+        self.r = (1./3.)*self.R
+        self.color = 'y'
+        self.mod = Toc(self.c,self.R)
+        return
+
+"""
+Shielding Module Object Class.
+"""
+class Shielding(object):
+    
+    """
+    Basic Constructor.
+    
+    @param: [center] center point of the module (Point).
+    @param: [R]      rib (length) of the module (int/float).
+    """
+    def __init__(self,center,R):
+        self.c = center
+        self.R = float(R)
+        self.label = 'Sh'
+        self.r = (1./3.)*self.R
+        self.color = ' '
+        self.mod = Toc(self.c,self.R)
+        return
+    
+"""
+Telemetry Module Object Class.
+
+@note: currently redundant.
+"""
+class Telemetry(object):
+    
+    """
+    Basic Constructor.
+    
+    @param: [center] center point of the module (Point).
+    @param: [R]      rib (length) of the module (int/float).
+    """
+    def __init__(self,center,R):
+        self.c = center
+        self.R = float(R)
+        self.label = 'T'
+        self.r = (1./3.)*self.R
+        self.color = 'k'
+        self.mod = Toc(self.c,self.R)
+        return
