353912ba2d046e011a93e503585e246f3b7e4fb7
atheys <A.Theys@student.tudelft.nl>
2017-05-14 04:18:43 +0200

    New fully incorporated update version of grid available now.

--- Deadalos/GridCreator.py
+++ Deadalos/GridCreator.py
@@ -2,14 +2,15 @@
 GridCreator Module.
 
 @author: Andreas Theys.
-@version: 1.2
+@version: 1.4
 """
 
 """
 Module imports.
 """
-from math import sqrt,floor,tan,radians
+from math import sqrt,floor,tan
 from BasicShapes import Point as P
+import GrowthMonitor as GM
 
 """
 Determines nearest even number.
@@ -32,9 +33,10 @@ Determines N-coefficient for number y- and z-cells.
 """
 def N(D,r):
     D,r = float(D),float(r)
-    coeff = (sqrt(2.)*D)/(2.8*r)
-    N = nearestUnevenNumber(int(floor(coeff)))
-    return N
+    coeff = D/r
+    N1 = nearestUnevenNumber(int(floor(coeff)))
+    N2 = nearestUnevenNumber(int(floor(1.5*float(N1))))
+    return N1,N2
 
 """
 Determines M-coefficient for number x-cells.
@@ -45,12 +47,12 @@ Determines M-coefficient for number x-cells.
 @return: [M]   M-coefficient (int).
 """
 def M(D,r,phi):
-    D,r,phi = float(D),float(r),min(float(phi),radians(9.736))
+    D,r,phi = float(D),float(r),float(phi)
     if phi <= 0.:
         return -1
     else:
-        coeff = ((2.*(1.-tan(phi))-sqrt(2.))/(2.8*tan(phi)*r))*D
-        M = nearestUnevenNumber(int(floor(coeff)))
+        coeff = (0.75*D)/(tan(phi)*r)
+        M = int(floor(coeff))
         return M
 
 """
@@ -62,14 +64,72 @@ Makes 3D grid matrix.
 @return: [grid] grid matrix (3D list strings).
 """      
 def makeGrid(D,r,phi):
-    n,m = N(D,r),M(D,r,phi)
-    row = n*['']
+    n1,n2 = N(D,r)
+    m = M(D,r,phi)
+    row = n2*['']
     matrix,grid = [],[]
-    for i in range(n):
+    for i in range(n1):
         matrix.append(row)
-    for i in range(m):
+    for i in range(m+n2/2):
         grid.append(matrix)
     return grid
+
+"""
+Makes 3D Starship boolean grid matrix.
+
+@param:  [D]    largest internal circular diameter (float/m).
+@param:  [r]    (uniform) rib length of the modules (float/m).
+@param:  [phi]  collision safety angle (float/radians).
+@return: [grid] boolean grid matrix (3D list strings).
+"""  
+def makeStarshipBooleanGrid(D,r,phi):
+    D,r,phi = float(D),float(r),float(phi)
+    grid = makeGrid(D,r,phi)
+    M,N1,N2 = len(grid),len(grid[0]),len(grid[0][0])
+    limits = GM.decreasingGrowthLine(grid)
+    mid1,mid2,bgrid = N1/2,N2/2,[]
+    for i in range(M):
+        temp1 = []
+        r = limits[i]
+        for j in range(N1):
+            temp2 = []
+            for k in range(N2):
+                r1,r2 = 2.*float(j-mid1)/float(N1),2.*float(k-mid2)/float(N2)
+                r3 = 2.*float(i-mid2)/float(N2)
+                e1,e2,e3 = bool(r1**2+r2**2<=r**2),r1**2+r2**2+r3**2>1.,i>mid2
+                e = bool(e1 and e2 and e3)
+                temp2.append(e)
+            temp1.append(temp2)
+        bgrid.append(temp1)       
+    return bgrid  
+    
+"""
+Makes 3D Asteroid boolean grid matrix.
+
+@param:  [D]    largest internal circular diameter (float/m).
+@param:  [r]    (uniform) rib length of the modules (float/m).
+@param:  [phi]  collision safety angle (float/radians).
+@return: [grid] boolean grid matrix (3D list strings).
+"""  
+def makeAsteroidBooleanGrid(D,r,phi):
+    D,r,phi = float(D),float(r),float(phi)
+    grid = makeGrid(D,r,phi)
+    M,N1,N2 = len(grid),len(grid[0]),len(grid[0][0])
+    limits = GM.decreasingGrowthLine(grid)
+    mid1,mid2,bgrid = N1/2,N2/2,[]
+    for i in range(M):
+        temp1 = []
+        r = limits[i]
+        for j in range(N1):
+            temp2 = []
+            for k in range(N2):
+                r1,r2 = 2.*float(j-mid1)/float(N1),2.*float(k-mid2)/float(N2)
+                r3 = 2.*float(i-mid2)/float(N2)
+                e = bool(r1**2+r2**2+r3**2<=1.)
+                temp2.append(e)
+            temp1.append(temp2)
+        bgrid.append(temp1)       
+    return bgrid 
 
 """
 Makes 3D grid matrix.
@@ -77,18 +137,20 @@ Makes 3D grid matrix.
 @param:  [D]    largest internal circular diameter (float/m).
 @param:  [r]    (uniform) rib length of the modules (float/m).
 @param:  [phi]  collision safety angle (float/radians).
-@return: [grid] grid point matrix (3D list Point-Objects).
+@return: [pgrid] grid point matrix (3D list Point-Objects).
 """      
 def makePointGrid(D,r,phi):
-    D,r,phi = float(D),float(r),min(float(phi),radians(9.736))
-    n,m,r2 = N(D,r),M(D,r,phi),(1./3.)*r
-    h,w,C = 0.25*sqrt(2.)*(r-r2),0.25*(r+r2),(n-1)/2
+    D,r,phi = float(D),float(r),float(phi)
+    grid = makeGrid(D,r,phi)
+    M,N1,N2 = len(grid),len(grid[0]),len(grid[0][0])
+    r2 = r/3.
+    h,w,C = 0.25*sqrt(2.)*(r-r2),0.25*(r+r2),(N2-1)/2
     pgrid = []
-    for i in range(m):
+    for i in range(M):
         temp1 = []
-        for j in range(n):
+        for j in range(N1):
             temp2 = []
-            for k in range(n):
+            for k in range(N2):
                 dj,dk = C-j,k-C
                 x,y,z = 2.*float(i)*w,2.*float(dk)*w,0.
                 if i%2==0:
@@ -105,6 +167,38 @@ def makePointGrid(D,r,phi):
             temp1.append(temp2)
         pgrid.append(temp1)
     return pgrid
+
+def updateGrid(grid,agrid,sgrid):
+    ugrid = []
+    for i in range(len(grid)):
+        temp1 = []
+        for j in range(len(grid[i])):
+            temp2 = []
+            for k in range(len(grid[i][j])):
+                if agrid[i][j][k]:
+                    temp2.append('Asteroid')
+                elif not(sgrid[i][j][k]):
+                    temp2.append('Edge')
+                else:
+                    temp2.append('')
+            temp1.append(temp2)
+        ugrid.append(temp1)
+    return ugrid
+
+def mined(n,grid,agrid):
+    i,j,k = n[0],n[1],n[2]
+    if agrid[i][j][k]:
+        grid[i][j][k] = ''
+        agrid[i][j][k] = False
+    return
+    
+def filled(n,symbol,grid,sgrid):
+    i,j,k = int(n[0]),int(n[1]),int(n[2])
+    if sgrid[i][j][k]:
+        grid[i][j][k] = str(symbol)
+        sgrid[i][j][k] = False
+    return
+    
    
 """
 Determines all neighboring nodes of certain grid point.
@@ -114,25 +208,26 @@ Determines all neighboring nodes of certain grid point.
 @return: [actual] actual list of neighboring nodes (list of tuples).
 """   
 def neighbors(n,grid,filled=False):
-    i,j,k = n[0],n[1],n[2]
-    nodes = [(i,j,k-1),(i+1,j,k),(i,j+1,k),(i-1,j,k),(i,j-1,k),\
-             (i+1,j-1,k),(i+1,j+1,k),(i-1,j+1,k),(i-1,j-1,k),\
-             (i+1,j,k+1),(i,j+1,k+1),(i-1,j,k+1),(i,j-1,k+1),(i,j,k+1)]
-    actual,numbers = [],[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-    for l in range(len(nodes)-1,-1,-1): 
+    i,j,k = int(n[0]),int(n[1]),int(n[2])
+    nodes = [ (i,j-1,k),(i+1,j,k),(i,j,k+1),(i-1,j,k),(i,j,k-1),\
+              (i+1,j,k-1),(i+1,j,k+1),(i-1,j,k+1),(i-1,j,k-1),\
+              (i+1,j+1,k),(i,j+1,k+1),(i-1,j+1,k),(i,j+1,k-1),(i,j+1,k)]
+    actual,numbers = [],[]
+    for l in range(len(nodes)): 
         node = nodes[l]
         try:
             x,y,z = node[0],node[1],node[2]
-            b = grid[x][y][z] != ''
+            b = bool(grid[x][y][z] == '')
             if x>=0 and y>=0 and z>=0:
                 if filled:
                     if b: 
                         actual.append(node)
-                        numbers.remove(l+1)
+                        numbers.append(l+1)
                 else:
                     actual.append(node)
+                    numbers.append(l+1)  
         except Exception:
-            temp = ' '
+            print 'OOPS'
     return actual,numbers
 
 """
